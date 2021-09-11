e2e762d287a33b1890cb1bf5dd5849fde303414c
miki081 <tgjmtp.kkmd125@gmail.com>
2021-09-11 14:12:32 +0000

    追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.(params[:id])
     @recipe = Recipe.new
     @event = Event.new
     @online = Online.new
